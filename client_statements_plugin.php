8f8468bd88120ed87594e7e75db53cae251516a8
Julian Matz <julianmatz@netlink.ie>
2016-04-07 07:22:23 +0100

    Add language/en_us/admin_company_emails.php

--- client_statements_plugin.php
+++ client_statements_plugin.php
@@ -46,4 +46,71 @@ class ClientStatementsPlugin extends Plugin {
 			),
 		);
 	}
+	
+	public function install( $plugin_id ) {
+		
+		Loader::loadModels( $this, array( "EmailGroups", "Emails" ) );
+		
+		$group = array(
+			'action' => "ClientStatements.send_statement",
+			'type' => "staff",
+			'plugin_dir' => "client_statements",
+			'tags' => "first_name,last_name",
+		);
+		
+		// Add the custom group
+		$group_id = $this->EmailGroups->add( $group );
+		
+		$email = array(
+			'email_group_id' => $group_id,
+			'company_id' => Configure::get( "Blesta.company_id" ),
+			'lang' => "en_us",
+			'from' => "no-reply@mydomain.com",
+			'from_name' => "My Company",
+			'subject' => "Subject of the email",
+			'text' => "Hi {first_name},
+			This is the text version of your email",
+			'html' => "<p>Hi {first_name},</p>
+			<p>This is the HTML version of your email</p>"
+		);
+		
+		// Add an email to the group
+		$this->Emails->add( $email );
+		
+	}
+	
+	public function uninstall( $plugin_id, $last_instance ) {
+		
+		Loader::loadModels( $this, array( "EmailGroups", "Emails" ) );
+		
+		// Fetch the email template created by this plugin
+		$group = $this->EmailGroups->getByAction( "ClientStatements.send_statement" );
+		
+		// Delete all emails templates belonging to this plugin's email group and company
+		if ( $group ) {
+			$this->Emails->deleteAll( $group->id, Configure::get( "Blesta.company_id" ) );
+		}
+		
+		if ( $last_instance ) {
+			
+			try {
+				// Remove the email template created by this plugin
+				if ( $group ) {
+					$this->EmailGroups->delete( $group->id );
+				}
+			}
+			catch ( Exception $e ) {
+				// Error dropping... no permission?
+				$this->Input->setErrors(
+					array(
+						'db' => array(
+							'create' => $e->getMessage()
+						)
+					)
+				);
+				return;
+			}
+		}
+	}
+	
 }
---
+++ language/en_us/admin_company_emails.php
@@ -0,0 +1,4 @@
+<?php
+
+$lang['AdminCompanyEmails.templates.ClientStatements.send_statement_name'] = "Client Account Statement";
+$lang['AdminCompanyEmails.templates.ClientStatements.send_statement_desc'] = "Client account statement email template.";
